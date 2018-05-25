f16f72a64e1117bebf00a4061cd0a211d89fb9f7
Daryl Segovia Alonzo <daryl_segovia@hotmail.com>
2018-05-24 21:10:17 -0500

    Archivos

---
+++ asset/Footer.php
@@ -0,0 +1,63 @@
+<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">From the blog</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Post Title 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Admin, domainname.com</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Friday, 6th April 2000</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Vestibulumaaccumsan egestibulum</a></li>
+                </ul>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Quick Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Latest Tweets</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <h5 class="white-text">Contact Us</h5>
+          <form action="envia.php" method="post" class="form-consulta"> 
+              <label>Full Name: <span>*</span>
+                <input type="text" name="nombre" placeholder="Nombre y apellido" class="campo-form" required="">
+              </label>
+              
+              <label>Email Address: <span>*</span>
+                <input type="email" name="email" placeholder="Email" class="campo-form" required="">
+              </label>
+              
+              <label>Subject:
+                <textarea name="consulta" class="campo-form"></textarea>
+              </label>
+
+              <label>
+                <textarea name="Message" class="campo-form"></textarea>
+              </label>
+
+              <input type="submit" value="Enviar" class="btn-form">
+            </form>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
--- asset/Index.php
+++ asset/Index.php
@@ -92,5 +92,115 @@
             </div>
         </div>
     </div>
+
+    <section id="portfolio" class="section-bg">
+      <div class="container">
+
+        <header class="section-header">
+          <h3 class="section-title">LATEST WORK</h3>
+        </header>
+
+        <div class="row portfolio-container" style="position: relative; height: 1080px;">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" style="position: absolute; left: 0px; top: 0px; visibility: visible; animation-name: fadeInUp;">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="img/Imagen1.jpg" class="img-fluid" alt="">
+                <a href="" data-lightbox="portfolio" data-title="App 1" class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
+                <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="#">App 1</a></h4>
+                <p>App</p>
+              </div>
+            </div>
+          </div>
+
+            <div class="row portfolio-container" style="position: relative; height: 1080px;">
+
+<div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" style="position: absolute; left: 0px; top: 0px; visibility: visible; animation-name: fadeInUp;">
+  <div class="portfolio-wrap">
+    <figure>
+      <img src="img/Imagen2.jpg" class="img-fluid" alt="">
+      <a href="" data-lightbox="portfolio" data-title="App 1" class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
+      <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+    </figure>
+
+    <div class="portfolio-info">
+      <h4><a href="#">App 1</a></h4>
+      <p>App</p>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s" style="position: absolute; left: 380px; top: 0px; visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+  <div class="portfolio-wrap">
+    <figure>
+      <img src="img/Imagen3.jpg" class="img-fluid" alt="">
+      <a href="" class="link-preview" data-lightbox="portfolio" data-title="Web 3" title="Preview"><i class="ion ion-eye"></i></a>
+      <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+    </figure>
+
+    <div class="portfolio-info">
+      <h4><a href="#">Web 3</a></h4>
+      <p>Web</p>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s" style="position: absolute; left: 760px; top: 0px; visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+  <div class="portfolio-wrap">
+    <figure>
+      <img src="img/Imagen4.jpg" class="img-fluid" alt="">
+      <a href="" class="link-preview" data-lightbox="portfolio" data-title="App 2" title="Preview"><i class="ion ion-eye"></i></a>
+      <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+    </figure>
+
+    <div class="portfolio-info">
+      <h4><a href="#">App 2</a></h4>
+      <p>App</p>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp" style="position: absolute; left: 0px; top: 360px; visibility: visible; animation-name: fadeInUp;">
+  <div class="portfolio-wrap">
+    <figure>
+      <img src="img/Imagen5.jpg" class="img-fluid" alt="">
+      <a href="" class="link-preview" data-lightbox="portfolio" data-title="Card 2" title="Preview"><i class="ion ion-eye"></i></a>
+      <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+    </figure>
+
+    <div class="portfolio-info">
+      <h4><a href="#">Card 2</a></h4>
+      <p>Card</p>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s" style="position: absolute; left: 380px; top: 360px; visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+  <div class="portfolio-wrap">
+    <figure>
+      <img src="img/Imagen6.jpg" class="img-fluid" alt="">
+      <a href="" class="link-preview" data-lightbox="portfolio" data-title="Web 2" title="Preview"><i class="ion ion-eye"></i></a>
+      <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+    </figure>
+
+    <div class="portfolio-info">
+      <h4><a href="#">Web 2</a></h4>
+      <p>Web</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<?php
+    include 'Footer.php';
+?>
+
 </body>
 </html>
