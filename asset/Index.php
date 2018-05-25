244608f7c9b0ad77e78a61f5f9f41120410fa3bd
Daryl Segovia Alonzo <daryl_segovia@hotmail.com>
2018-05-24 19:52:23 -0500

    Archivos php

---
+++ asset/Index.php
@@ -0,0 +1,96 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Pérfiles yucatecos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--CSS-->
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/animations.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/icon.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/materialize.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/materialize.min.css" rel="stylesheet" />
+
+    <!--JavaScripts-->
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/jquery.mask.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/jquery.mask.min.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/jquery-2.1.4.min.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/materialize.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/materialize.min.js"></script>
+
+</head>
+<body>
+    <?php
+        include 'Navbar.php';
+    ?>
+
+    <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">SERVICE TITLE</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p class="card-text">Vestassapede et donec ut estlibe ros sus et eget sed eget quisq ueta habitur augue</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">★ ★ ★ ★ ☆</small>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">SERVICE TITLE</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p class="card-text">Vestassapede et donec ut estlibe ros sus et eget sed eget quisq ueta habitur augue</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">★ ★ ★ ★ ☆</small>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">SERVICE TITLE</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p class="card-text">Vestassapede et donec ut estlibe ros sus et eget sed eget quisq ueta habitur augue</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">★ ★ ★ ★ ☆</small>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">SERVICE TITLE</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p class="card-text">Vestassapede et donec ut estlibe ros sus et eget sed eget quisq ueta habitur augue</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">★ ★ ★ ★ ☆</small>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ asset/Navbar.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Práctica 9 - Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--CSS-->
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/animations.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/icon.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/materialize.css" rel="stylesheet" />
+    <link href="../PracticaGrupal/PerfilesYucatecos/asset/css/materialize.min.css" rel="stylesheet" />
+
+    <!--JavaScripts-->
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/jquery.mask.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/jquery.mask.min.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/jquery-2.1.4.min.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/materialize.js"></script>
+    <script src="../PracticaGrupal/PerfilesYucatecos/asset/js/materialize.min.js"></script>
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper" style="background-color:#000000; color:#747474;">
+      <a href="#" class="brand-logo">The modernist</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">HOME</a></li>
+        <li><a href="badges.html">STYLE DEMO</a></li>
+        <li><a href="collapsible.html">FULL WIDTH</a></li>
+        <li><a href="collapsible.html">DROPDOWN</a></li>
+        <li><a href="collapsible.html">PORTAFOLIO</a></li>
+        <li><a href="null.html">GALLERY</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</body>
+</html>
