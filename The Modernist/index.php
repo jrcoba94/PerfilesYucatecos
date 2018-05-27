117f09159c2cbab510b17eb9129cae711678e08b
JOSE_COBA <jr.coba94@gmail.com>
2018-05-27 14:24:25 -0500

    reop

---
+++ The Modernist/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<!-- saved from url=(0040)https://scubior.github.io/the-modernist/ -->
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Modernist</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="./css/materialize.css">
+    <link rel="stylesheet" type="text/css" href="./iconfont/material-icons.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+
+
+    <script src="./js/jquery-2.1.4.min.js"></script>
+    <script src="./js/materialize.js"></script>
+    <script src="./Modernist_files/jquery.scrollTo.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.slider').slider();
+        });
+    </script>
+</head>
+
+<body>
+    <div class="contentt-box">
+        <div class="baner-box">
+            <?php include 'navbar.php'; ?>
+
+            <div class="slider">
+                <ul class="slides" style="background:transparent;">
+                    <li>
+                <div class="slider-box">
+                    <div class="slider-text">
+                    <p>Cursus penati saccum nulla.</p>
+                    <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque in mauris et vestibulum. Fusce luctus, tellus in vestibulum pretium, diam elit sagittis est, porta semper quam elit at quam.</small>
+                    <button type="button">Read more here <span>&raquo;</span></button>
+                    </div>
+                    <div class="slider-box-img">
+                        <div class="slider-img img-sepia"></div>
+                    </div>
+                </div>
+    </li>
+    <li>
+                <div class="slider-box">
+                    <div class="slider-text">
+                    <p>Cursus penati saccum nulla.</p>
+                    <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque in mauris et vestibulum. Fusce luctus, tellus in vestibulum pretium, diam elit sagittis est, porta semper quam elit at quam.</small>
+                    <button type="button">Read more here <span>&raquo;</span></button>
+                    </div>
+                    <div class="slider-box-img">
+                        <div class="slider-img"></div>
+                    </div>
+                </div>
+    </li>
+    <li>
+                <div class="slider-box">
+                    <div class="slider-text">
+                    <p>Cursus penati saccum nulla.</p>
+                    <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque in mauris et vestibulum. Fusce luctus, tellus in vestibulum pretium, diam elit sagittis est, porta semper quam elit at quam.</small>
+                    <button typ="button">Read more here <span>&raquo;</span></button>
+                    </div>
+                    <div class="slider-box-img">
+                        <div class="slider-img img-grayscale"></div>
+                    </div>
+                </div>
+    </li>
+                </ul>
+            </div>
+            
+        </div>
+
+
+        <div class="col-s-12 services">
+            <div class="services-content">
+                <i class="large material-icons" aria-hidden="true">wb_sunny</i>
+                <p>service title</p> 
+                <small>Cras imperdiet felis vel lectus commodo pharetra. Aenean consectetur nec nisl id luctus. </small>
+            </div>
+            <div class="services-content">
+            <i class="large material-icons" aria-hidden="true">wb_sunny</i>
+                <p>service title</p>
+                <small>Cras imperdiet felis vel lectus commodo pharetra. Aenean consectetur nec nisl id luctus. </small></div>
+            <div class="services-content">
+            <i class="large material-icons" aria-hidden="true">wb_sunny</i>
+                <p>service title</p>
+                <small>Cras imperdiet felis vel lectus commodo pharetra. Aenean consectetur nec nisl id luctus. </small></div>
+            <div class="services-content">
+            <i class="large material-icons" aria-hidden="true">wb_sunny</i>
+                <p>service title</p>
+                <small>Cras imperdiet felis vel lectus commodo pharetra. Aenean consectetur nec nisl id luctus. </small></div>
+        </div>    
+
+
+
+        <div class="latestwork">
+            <div class="latestwork-btn">latest work</div>
+            <div class="latestwork-box-img">
+                <div class="latestwork-img"><img class="responsive-img" src="./img/imagen1.jpg" alt=""/></div>
+                <div class="latestwork-img"><img class="responsive-img" src="./img/imagen2.jpg" alt="" /></div>
+                <div class="latestwork-img"><img class="responsive-img" src="./img/imagen3.jpg" alt="" /></div>
+                <div class="latestwork-img"><img class="responsive-img" src="./img/imagen1.jpg" alt="" /></div>
+                <div class="latestwork-img"><img class="responsive-img" src="./img/imagen6.jpg" alt="" /></div>
+                <div class="latestwork-img"><img class="responsive-img" src="./img/imagen5.jpg" alt="" /></div>
+            </div>
+        </div>
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
